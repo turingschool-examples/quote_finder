9b15785020c3d139b72b02a920c2d04571e05e7b
Dani Coleman <danicoleman00@gmail.com>
2022-10-24 13:11:49 -0600

    Adds Welcome directory and index_spec.rb

---
+++ spec/features/Welcome/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Landing Page' do
+  it "can fill in form with a keyword, hit submit and redirect to quotes index" do
+    visit root_path
+
+    fill_in :keyword, with: 'I am'
+
+    click_on "Search for Quotes"
+
+    expect(current_path).to eq("/quotes")
+  end
+end
