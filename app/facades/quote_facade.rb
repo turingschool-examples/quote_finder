ff9d62296618c81da36aee5f0770dfa95223ccd7
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-28 10:35:51 -0600

    feat: add view logic to display author, content, and tags

--- app/facades/quote_facade.rb
+++ app/facades/quote_facade.rb
@@ -1,9 +1,8 @@
 class QuoteFacade
- 
+
  def self.quote_search(search_params)
   search_data = QuoteService.search(search_params)
   search = search_data[:results]
-  end
   search.take(10)
  end
 end
