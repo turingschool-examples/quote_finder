6470cd8a016c8f6a0419f546c00a58e0ac632a9f
Aleisha Mork <aleisha.mork@gmail.com>
2022-10-28 10:04:11 -0600

    Feat: Add formating and convert to objects functionality to QuoteFacade

--- app/facades/quote_facade.rb
+++ app/facades/quote_facade.rb
@@ -1,5 +1,10 @@
 class QuoteFacade
   def self.quotes_by_keyword(keyword)
-    quote_data = QuoteService.get_10_quotes_by_keyword(keyword)
+    quotes_data = QuoteService.get_10_quotes_by_keyword(keyword)
+    quotes = {total_quotes: quotes_data[:totalCount], quotes: []}
+    quotes[:quotes] = quotes_data[:results].map do |quote_data|
+      Quote.new(quote_data)
+    end
+    quotes
   end
 end
