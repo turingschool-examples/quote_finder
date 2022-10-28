a72af3f94bc47291352826cd7201929b35f5a3cc
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-28 10:04:51 -0600

    test: user story skeleton test written

--- spec/features/Welcome/index_spec.rb
+++ spec/features/Welcome/index_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe 'Landing Page' do
 
     expect(current_path).to eq("/quotes")
   end
+
+  describe "I am taken to '/quotes' where I see the TOTAL number of quotes that were found for that keyword" do
+    describe "As well as the keyword that's being used for the search" do
+      it "As well as a list of the top 10 quotes that were found" do
+        visit root_path
+
+        fill_in :keyword, with: 'Love'
+
+        click_on "Search for Quotes"
+
+        expect(current_path).to eq("/quotes")
+
+        expect(page).to have content('Quotes Matching Search For:')
+      end
+    end
+  end
 end
