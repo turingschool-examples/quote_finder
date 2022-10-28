926f73ebac95eee9efd0ef20758a0405e49cf63f
Sid Mann <sjmann2@outlook.com>
2022-10-28 10:53:34 -0500

    feat quotes service

---
+++ app/services/quotes_service.rb
@@ -0,0 +1,10 @@
+class QuotesService
+  def self.conn
+    Faraday.new(url: 'https://api.quotable.io')
+  end
+
+  def self.search_by_keyword(keyword)
+    response = conn.get("/search/quotes?query=#{keyword}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
