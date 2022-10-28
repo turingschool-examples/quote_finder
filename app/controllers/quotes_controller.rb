aec952322456c3f4d1dfce9ef20fea07dd71e0ab
Sid Mann <sjmann2@outlook.com>
2022-10-28 11:10:31 -0500

    Add total count

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,6 @@
 class QuotesController < ApplicationController
   def index
     @quotes = QuotesFacade.search_by_keyword(params[:query])
+    @total_count = QuotesFacade.total_count_by_keyword(params[:query])
   end
 end
