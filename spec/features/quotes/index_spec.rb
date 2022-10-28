c2f586267ed62e24239e3f18a09611a34be2ebb5
nicole-esquer <nicole.esquer5@gmail.com>
2022-10-28 12:14:08 -0700

    complete testing for quotes index page

--- spec/features/quotes/index_spec.rb
+++ spec/features/quotes/index_spec.rb
@@ -9,7 +9,10 @@ RSpec.describe 'Quote Index Page' do
     click_on 'Search for Quotes'
 
     expect(current_path).to eq(quotes_path)
+    expect(page).to have_content('Search Results')
     expect(page).to have_content('Total Count: 6')
+    expect(page).to have_content('Results on this page: 6')
+    expect(page).to have_content('Quote Matches')
   end
 
   it 'displays 10 quotes that match the search' do
@@ -18,5 +21,8 @@ RSpec.describe 'Quote Index Page' do
     fill_in :keyword, with: 'shadows'
 
     click_on 'Search for Quotes'
+
+    expect(page).to have_content('Author: Abraham Lincoln')
+    expect(page).to have_content('Category: famous-quotes')
   end
 end
