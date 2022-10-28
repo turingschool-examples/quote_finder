820813869cbfee8276aed366bd7dfcf0499e1dc9
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-28 10:14:25 -0600

    feat: add initial connection with missing query

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,6 @@
 class QuotesController < ApplicationController
  def search
-  
+  response = Faraday.get('https://api.quotable.io/search/quotes')
+  @quotes = JSON.parse(response.body, symbolize_names: true)
  end
 end
