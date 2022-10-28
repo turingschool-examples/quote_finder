a927fa44c76229d5ab3368bd16f897bdc513e3f8
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-10-28 08:43:49 -0700

    Add test for user story 1

--- spec/features/Welcome/index_spec.rb
+++ spec/features/Welcome/index_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe 'Landing Page' do
 
     expect(current_path).to eq("/quotes")
   end
+
+  it "shows a list of all quotes for keyword" do
+    visit root_path
+
+    fill_in :keyword, with: 'Sagan'
+    click_on "Search for Quotes"
+
+    expect(page).to have_content 'Quotes for Sagan'
+    expect(page).to have_content 'Total Number of Quotes: 1'
+    expect(page).to have_content 'Author: Carl Sagan'
+    expect(page).to have_content 'Categories: famous-quotes'
+
+    #within block for number of quotes 10?
+    #expect(page).to_not have_content
+  end
 end
