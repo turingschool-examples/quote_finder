b9893ff720c0a644dbe7e4048a6aa9dc65d11f44
Sid Mann <sjmann2@outlook.com>
2022-10-28 10:35:10 -0500

    test quotes service

--- spec/services/quotes_service_spec.rb
+++ spec/services/quotes_service_spec.rb
@@ -3,8 +3,15 @@ require 'rails_helper'
 RSpec.describe QuotesService do
   it 'returns an HTTP response of quotes by keyword' do
     quotes = QuotesService.search_by_keyword("I am")
-    require 'pry' ; binding.pry
+
     expect(quotes.count).to eq(151)
-    expect(quotes)
+    expect(quotes).to be_a(Hash)
+    expect(quotes).to have_key(:totalCount)
+    expect(quotes[:totalCount]).to be_a(Integer)
+    expect(quotes[:results]).to be_an(Array)
+    expect(quotes[:results]).to have_key(:content)
+    expect(quotes[:results][:content]).to be_a(String)
+    expect(quotes[:results]).to have_key(:author)
+    expect(quotes[:results][:author]).to be_a(String)
   end
 end
