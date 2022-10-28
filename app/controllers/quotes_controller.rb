98d5c3d758d522b7f36f3ad7795c907603181c40
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-10-28 13:36:12 -0700

    Add most of user story 1 functionality

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,6 @@
 class QuotesController < ApplicationController
     def index
+        @data = QuoteService.quote_search(params[:query])
         @quotes = QuoteFacade.get_quotes(params[:query])
     end
 end
