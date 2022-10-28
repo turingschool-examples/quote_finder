018ff49bd446bfed5f5f1c0351addff0fcc15f82
Stephen Fabian <fabianstephen@gmail.com>
2022-10-28 10:29:03 -0600

    additional testing

--- spec/features/Welcome/index_spec.rb
+++ spec/features/Welcome/index_spec.rb
@@ -12,6 +12,26 @@ RSpec.describe 'Landing Page' do
   end
 
   it "I see total number of quotes that were found for that keyword" do
+
+    output = {
+    "count": 1,
+    "results": [
+        {
+            "_id": "JIk3cbQ8s",
+            "author": "Michael Jordan",
+            "content": "I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times, I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.",
+            "tags": [
+                "sports",
+                "competition",
+                "famous-quotes"
+            ],
+            "authorSlug": "michael-jordan",
+            "length": 224,
+            "dateAdded": "2022-07-06",
+            "dateModified": "2022-07-08"
+        }]}
+
+    allow(QuotableService).to receive(:search).and_return(output)
     visit root_path
 
     fill_in :keyword, with: 'I am'
@@ -21,5 +41,8 @@ save_and_open_page
     expect(page).to have_content "Number of quotes found:"
     expect(page).to have_content "Search keyword(s): I am"
 
+
   end
+
+
 end
