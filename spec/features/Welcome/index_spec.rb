808e408234abb82a3658c9452dbc1f6b5df1ff30
dominicod <dominicodonnell99@gmail.com>
2022-10-28 10:11:22 -0500

    Feat: run rubocop

--- spec/features/Welcome/index_spec.rb
+++ spec/features/Welcome/index_spec.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Landing Page' do
-  it "can fill in form with a keyword, hit submit and redirect to quotes index" do
+  it 'can fill in form with a keyword, hit submit and redirect to quotes index' do
     visit root_path
 
     fill_in :keyword, with: 'I am'
 
-    click_on "Search for Quotes"
+    click_on 'Search for Quotes'
 
-    expect(current_path).to eq("/quotes")
+    expect(current_path).to eq('/quotes')
   end
 end
