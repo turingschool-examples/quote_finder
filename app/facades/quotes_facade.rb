1b31cebd1af1673f0306742015eac15acae3f1da
dominicod <dominicodonnell99@gmail.com>
2022-10-28 11:46:08 -0500

    Finish user story 1

--- app/facades/quotes_facade.rb
+++ app/facades/quotes_facade.rb
@@ -6,6 +6,8 @@ class QuotesFacade
     arr = response[:results].map do |quote|
       Quote.new(quote)
     end
+    # Want to know if there is a better way of doing this, as this is a bit hacky. However my reasoning is I don't want to create another
+    # facade method to hit the API again jsut to grab the total count, so for time complexity I choose to use an arr
     [arr, response[:totalCount]]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'quotes', to: 'quotes#index'
+  # Where we suppose to only have '/quotes' in the url or is the param query string okay?
+  # If not, how do I remove the query string from url
 end
