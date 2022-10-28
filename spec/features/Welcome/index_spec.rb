4cddb5fa984a5f253a9b20cd9de44c3dc5e4fd54
Sid Mann <sjmann2@outlook.com>
2022-10-28 11:40:35 -0500

    test edge cases

--- spec/features/Welcome/index_spec.rb
+++ spec/features/Welcome/index_spec.rb
@@ -40,27 +40,11 @@ RSpec.describe 'Landing Page' do
       fill_in :keyword, with: 'I am an optimist'
 
       click_on "Search for Quotes"
-      
+
       expect(page).to have_content(quote.author)
       expect(page).to have_content(quote.quote)
+
       expect(page).to_not have_content(quote.categories)
     end
   end
 end
-# As a visitor
-# When I visit the landing page '/'
-# And fill in the form with a keyword 
-# And hit 'Search for Quotes'
-# I am taken to '/quotes'
-# Where I see the TOTAL number of quotes that were found for that keyword
-# As well as the keyword that's being used for the search
-# As well as a list of the top 10 quotes that were found
-
-# For each quote listed, I should see: 
-# - The quote's author
-# - The quote's category(ies) (i.e. tags)
-# ```
-
-# Notes:
-# - If there are less than 10 quotes found for a particular search, then only return the quotes that were found. 
-# - If the quote has no tags associated with it, do not list any categories for that quote.
