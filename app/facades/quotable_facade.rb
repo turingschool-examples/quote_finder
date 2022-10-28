ac1a5981a5c98f76bde3f26ed0d44309b8700c40
Riley McCullough <rileybmccullough@gmail.com>
2022-10-28 11:02:47 -0600

    Fixed edge case: search returns zero results

--- app/facades/quotable_facade.rb
+++ app/facades/quotable_facade.rb
@@ -3,7 +3,11 @@ class QuotableFacade
   def self.search_for_quotes(query)
     # require "pry"; binding.pry
     json = QuotableService.search_quotes(query)
-    json[:results][0..9].map {|quote| Quote.new(quote, json[:count])}
+    if json[:results].count > 0
+      json[:results][0..9].map {|quote| Quote.new(quote, json[:count])}
+    else
+      [Quote.new({results: {content: [], tags: [], author: []}}, json[:count])]
+    end
 
   end
 
