05395ba0b51284d800b411f47a28803486e2937a
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-10-28 09:21:46 -0700

    Create quote facade spec

---
+++ spec/facades/quote_facade_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'QuoteFacade' do
+  it 'makes a call and returns an array of parks in a state' do
+    quotes = QuoteFacade.get_quotes("Sagan")
+
+    expect(quotes).to be_an(Array)
+    expect(quotes).to be_all(Quote)
+  end
+end
